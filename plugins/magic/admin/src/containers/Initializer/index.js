28f0f101cf70d6b3ff0f61d7f201ba85c4361075
Entreprenerd <valeranijr@gmail.com>
2020-10-19 13:10:28 +0200

    fix plugin works post publishing

--- plugins/magic/admin/src/containers/Initializer/index.js
+++ plugins/magic/admin/src/containers/Initializer/index.js
@@ -6,7 +6,7 @@
 
 import { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import pluginId from '../../../../pluginId';
+import pluginId from '../../pluginId';
 
 const Initializer = ({ updatePlugin }) => {
   const ref = useRef();
